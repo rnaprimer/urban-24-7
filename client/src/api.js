72df8a3c8f3ae8243f22df84374fbafc2bb8fbf1
rnaprimer <debashismohanty5714@gmail.com>
2025-12-04 20:14:55 +0530

    Prepare for deployment: Use env vars for Port and API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+
 const api = axios.create({
-    baseURL: '/api'
+    baseURL: API_URL
 });
 
 export const getProfessionals = (params) => api.get('/professionals', { params });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { db, initDb } = require('./db');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
