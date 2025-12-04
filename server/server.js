ac695d5c3d4c5178b1c847262c08572b4a3a9880
rnaprimer <debashismohanty5714@gmail.com>
2025-12-04 20:35:39 +0530

    Configure for Render deployment: Monolith setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { db, initDb } = require('./db');
 
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
